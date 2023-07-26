85d488a2d12d3de390bafc9b59830fecc196cdf1
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-07-26 15:40:41 -0400

    Creacion front e importacion de librerias.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          Learn React
+      <button className="btn btn-primary"><i class="fa-solid fa-plus"></i></button>
+    </div>
+  );
+}
+
+export default App;
